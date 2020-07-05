9d62ee538d87adea54bd14bb170bf7939e5b78c6
Cory Reed <swashcap@gmail.com>
2020-07-05 00:16:33 -0700

    feat(text): add variant sizes

--- src/Text/Text.stories.tsx
+++ src/Text/Text.stories.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
 
-import { Text as TextComponent } from './Text';
+import { Text } from './Text';
 
 export default {
   title: 'Component|Text',
 };
 
-export const Text = () => <TextComponent>Text</TextComponent>;
+export const Default = () => <Text>Text</Text>;
+
+export const As = () => (
+  <>
+    <Text as="h1">h1</Text>
+    <Text as="h2">h2</Text>
+    <Text as="p">p</Text>
+    <Text as="small">small</Text>
+  </>
+);
+
+export const Variant = () => (
+  <>
+    <Text as="h1" variant="title">
+      title
+    </Text>
+    <Text as="h2" variant="headline-1">
+      headline-1
+    </Text>
+    <Text as="h3" variant="headline-2">
+      headline-2
+    </Text>
+    <Text as="h4" variant="headline-3">
+      headline-3
+    </Text>
+    <Text as="h5" variant="subhead-1">
+      subhead-1
+    </Text>
+    <Text as="h6" variant="subhead-2">
+      subhead-2
+    </Text>
+    <Text as="p" variant="body">
+      body
+    </Text>
+    <Text as="small" variant="caption">
+      caption
+    </Text>
+  </>
+);
--- src/Text/Text.tsx
+++ src/Text/Text.tsx
@@ -1,12 +1,64 @@
 import React from 'react';
 
+import styles from './Text.module.css';
 import { clsx } from '../utils/clsx';
 
-export type TextProps = React.DetailedHTMLProps<
-  React.HTMLAttributes<HTMLSpanElement>,
-  HTMLSpanElement
->;
+export type TextVariant =
+  | 'title'
+  | 'headline-1'
+  | 'headline-2'
+  | 'headline-3'
+  | 'subhead-1'
+  | 'subhead-2'
+  | 'body'
+  | 'caption';
 
-export const Text: React.FC<TextProps> = ({ className, ...rest }) => (
-  <span className={clsx('Text', className)} {...rest} />
-);
+export type TextElement =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'p'
+  | 'small'
+  | 'span';
+
+export type TextProps<T extends TextElement> = React.HTMLAttributes<
+  HTMLElement
+> & {
+  as?: T;
+  /**
+   * The text's size/type variation.
+   *
+   * @default 'body'
+   */
+  variant?: TextVariant;
+};
+
+export const Text = <T extends TextElement = 'span'>({
+  as,
+  className,
+  variant,
+  ...rest
+}: TextProps<T>) => {
+  const Component = as ?? 'span';
+
+  return (
+    <Component
+      className={clsx(
+        styles.Text,
+        variant === 'body' && styles.TextBody,
+        variant === 'caption' && styles.TextCaption,
+        variant === 'headline-1' && styles.TextHeadline1,
+        variant === 'headline-2' && styles.TextHeadline2,
+        variant === 'headline-3' && styles.TextHeadline3,
+        variant === 'subhead-1' && styles.TextSubhead1,
+        variant === 'subhead-2' && styles.TextSubhead2,
+        variant === 'title' && styles.TextTitle,
+        className
+      )}
+      {...rest}
+    />
+  );
+};
