118c6b869495a05d6521c1994176820799de9b89
Cory Reed <swashcap@gmail.com>
2020-08-22 13:04:39 -0700

    docs(storybook): add story for space

---
+++ .storybook/stories/space.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Text } from '../../src';
+
+export default {
+  title: 'Design|Space',
+};
+
+const Space: React.FC<{ value: string }> = ({ value }) => (
+  <tr>
+    <th scope="row" style={{ padding: 'var(--space-100)' }}>
+      <Text variant="caption">{`space-${value}`}</Text>
+    </th>
+    <td style={{ padding: 'var(--space-100)' }}>
+      <div
+        style={{
+          background: 'var(--theme-color-primary-background)',
+          height: `var(--space-${value})`,
+          width: `var(--space-${value})`,
+        }}
+      />
+    </td>
+  </tr>
+);
+
+export const Default = () => (
+  <table
+    style={{
+      borderCollapse: 'collapse',
+    }}
+  >
+    <tbody>
+      <Space value="50" />
+      <Space value="100" />
+      <Space value="200" />
+      <Space value="300" />
+      <Space value="400" />
+    </tbody>
+  </table>
+);
