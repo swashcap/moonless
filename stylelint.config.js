2594eb9dcaf0ca7efd48f91635b313d4d9ff2d98
Cory Reed <swashcap@gmail.com>
2020-09-20 23:49:02 -0700

    chore: add custom CSS property safeguard plugin

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-css-modules'],
-  plugins: ['stylelint-order'],
+  plugins: ['stylelint-order', 'stylelint-value-no-unknown-custom-properties'],
+  rules: {
+    'csstools/value-no-unknown-custom-properties': [
+      true,
+      {
+        importFrom: [
+          'src/tokens/color.css',
+          'src/tokens/space.css',
+          'src/tokens/typography.css',
+          'src/styles/theme.css',
+        ],
+      },
+    ],
+  },
 };
